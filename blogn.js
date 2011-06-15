d2d1f903e8625259d5ce211e5f53def5984f755d
edlong <e.i.a.longman@gmail.com>
2011-06-15 20:51:28 +0000

    added db support hopefully :)

--- blogn.js
+++ blogn.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var db = require('db');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('Hello World');
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end('Hello World');
 }).listen(process.env.C9_PORT);
 
 console.log('Server running at http://blogn.edlong.c9.io');
