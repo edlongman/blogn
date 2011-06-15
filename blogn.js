82da33e4d8ff85ea99a60c8c0d861d96d3b23e5e
edlong <e.i.a.longman@gmail.com>
2011-06-15 20:29:35 +0000

    added basics

---
+++ blogn.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('Hello World');
+}).listen(process.env.C9_PORT);
+
+console.log('Server running at http://blogn.edlong.c9.io');
